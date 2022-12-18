1845d4e80deec42c107f1b71ca9bb25743b2a67a
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2022-12-18 19:39:40 +0200

    Added db create/drop commands

--- source/odood/cli/database.d
+++ source/odood/cli/database.d
@@ -5,7 +5,7 @@ private import std.format: format;
 private import std.exception: enforce;
 
 private import thepath: Path;
-private import commandr: Option, Flag, ProgramArgs;
+private import commandr: Argument, Option, Flag, ProgramArgs;
 
 private import odood.cli.command: OdoodCommand;
 private import odood.lib.project: Project, ProjectConfig;
@@ -28,10 +28,50 @@ class CommandDatabaseList: OdoodCommand {
 }
 
 
+class CommandDatabaseCreate: OdoodCommand {
+    this() {
+        super("create", "Create new odoo database.");
+        this.add(new Option(
+            "l", "lang",
+            "Language of database, specified as ISO code of language."
+        ).defaultValue("en_US"));
+        this.add(new Option(
+            null, "password", "Admin password for this database."));
+        this.add(new Option(
+            null, "country", "Country for this db."));
+        this.add(new Flag("d", "demo", "Load demo data for this db"));
+        this.add(new Argument("name", "Name of database").required());
+    }
+
+    public override void execute(ProgramArgs args) {
+        auto project = new Project();
+        project.lodoo.createDatabase(
+            args.arg("name"),
+            args.flag("demo"),
+            args.option("lang"),
+            args.option("password"),
+            args.option("country"));
+    }
+}
+
+class CommandDatabaseDrop: OdoodCommand {
+    this() {
+        super("drop", "Drop the odoo database.");
+        this.add(new Argument("name", "Name of database").required());
+    }
+
+    public override void execute(ProgramArgs args) {
+        auto project = new Project();
+        project.lodoo.dropDatabase(args.arg("name"));
+    }
+}
+
 class CommandDatabase: OdoodCommand {
     this() {
         super("db", "Database management commands");
         this.add(new CommandDatabaseList());
+        this.add(new CommandDatabaseCreate());
+        this.add(new CommandDatabaseDrop());
     }
 }
 
--- source/odood/lib/odoo/lodoo.d
+++ source/odood/lib/odoo/lodoo.d
@@ -3,7 +3,7 @@ module odood.lib.odoo.lodoo;
 private import thepath: Path;
 
 private import odood.lib.project: ProjectConfig;
-private import odood.lib.utils: runCmdE;
+private import odood.lib.utils: runCmd, runCmdE;
 
 
 /** Wrapper struct around [LOdoo](https://pypi.org/project/lodoo/)
@@ -15,6 +15,18 @@ const struct LOdoo {
         ProjectConfig _config;
 
         Path _lodoo_path;
+
+        /// Run lodoo with provided args
+        auto run(in string[] args...) {
+            return _lodoo_path.runCmd(["--conf", _odoo_conf.toString] ~ args);
+        }
+
+        /** Run lodoo with provided args, raising error
+          * in case of non-zero exit status.
+          **/
+        auto runE(in string[] args...) {
+            return _lodoo_path.runCmdE(["--conf", _odoo_conf.toString] ~ args);
+        }
 
     public:
         @disable this();
@@ -30,11 +42,39 @@ const struct LOdoo {
         string[] listDatabases() {
             import std.array: split, array;
             import std.algorithm.iteration: filter;
-            auto res = _lodoo_path.runCmdE([
-                "--conf", _odoo_conf.toString, "db-list"]);
-
+            auto res = runE("db-list");
             return res.output.split("\n").filter!(db => db && db != "").array;
         }
 
+        /** Create new Odoo database on this odoo instance
+          **/
+        void createDatabase(in string name, in bool demo=false,
+                            in string lang=null, in string password=null,
+                            in string country=null) {
+            string[] args = ["db-create"];
 
+            if (demo)
+                args ~= ["--demo"];
+            else
+                args ~= ["--no-demo"];
+
+            if (lang)
+                args ~= ["--lang", lang];
+
+            if (password)
+                args ~= ["--password", password];
+
+            if (country)
+                args ~= ["--country", country];
+
+            args ~= [name];
+
+            runE(args);
+        }
+
+        /** Drop specified database
+          **/
+        void dropDatabase(in string name) {
+            runE("db-drop", name);
+        }
 }
