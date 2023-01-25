d59eedd243139fce6355b6726c26e6e80e1b40bb
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2023-01-25 21:59:24 +0200

    Added ability to build bash-completition script

--- source/odood/app.d
+++ source/odood/app.d
@@ -11,6 +11,19 @@ import odood.cli.app;
   **/
 int main(string[] args) {
     auto program = new App();
-    return program.run(args);
+
+    version(odood_bash_autocomplete) {
+        /* In case we need to generate bash autocompletition script,
+         * we just want to print it to stdout. Letter it will be included in
+         * debian package.
+         */
+        import commandr.completion.bash;
+
+        write(program.createBashCompletionScript());
+        return 0;
+    } else {
+        // Just run the program
+        return program.run(args);
+    }
 }
 
