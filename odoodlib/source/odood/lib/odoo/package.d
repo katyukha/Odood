615ba979d0883adaf484fb8f1a1b0db996ec2f0b
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2023-02-21 21:44:11 +0200

    Move initialization of pyd to odood.lib package

--- odoodlib/source/odood/lib/odoo/package.d
+++ odoodlib/source/odood/lib/odoo/package.d
@@ -1,8 +1,1 @@
 module odood.lib.odoo;
-
-private import pyd.def: py_init;
-
-// Initialize pyd as early as possible.
-shared static this() {
-    py_init();
-}
--- odoodlib/source/odood/lib/package.d
+++ odoodlib/source/odood/lib/package.d
@@ -3,3 +3,10 @@ module odood.lib;
 public immutable string _version = "0.0.1";
 
 public import odood.lib.project;
+
+
+// Initialize pyd as early as possible.
+shared static this() {
+    import pyd.def: py_init;
+    py_init();
+}
