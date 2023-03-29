4c14c9b0ddabd9d8e7d1ed88b7c809e10cf47415
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2023-03-29 21:24:30 +0300

    odood.lib.signal: Make it thread-safe
    Also, make public interface of odood.lib.signal @safe

--- subpackages/lib/source/odood/lib/signal.d
+++ subpackages/lib/source/odood/lib/signal.d
@@ -4,31 +4,45 @@ private import core.sys.posix.signal;
 
 private import std.logger;
 private import std.exception;
+private import core.atomic : atomicOp, atomicLoad, atomicStore;
 
 private import odood.lib.exception: OdoodException;
 
 private:
-    __gshared bool _interrupted = false;
-    __gshared bool _sigint_handler_set = false;
+    shared bool _interrupted = false;
+    shared uint _sigint_handler_counter = 0;
 
-    sigaction_t oldSigIntAction;
+    __gshared sigaction_t oldSigIntAction;
 
 	extern(C) void _sigIntHandler(int sig_number) nothrow {
-		_interrupted = true;
+		_interrupted.atomicStore(true);
 	}
 
-public:
-
-    /** This exception will be raised on attempt to initialize signal handling
-      * when it is already initialized
+    /** Register SIGINT Handler.
+      *
+      * Private implementation.
       **/
-    class SignalHandlerAlreadySetError : OdoodException {
-        mixin basicExceptionCtors;
+    @trusted nothrow void _registerSigIntHandler() {
+        sigaction_t handler;
+        handler.sa_handler = &_sigIntHandler;
+        handler.sa_mask = cast(sigset_t) 0;
+        handler.sa_flags = 0;
+        handler.sa_flags |= SA_RESTART;
+        sigaction(SIGINT, &handler, &oldSigIntAction);
     }
 
+    /** Unregister SIGINT Handler.
+      *
+      * Private implementation.
+      **/
+    @trusted nothrow void _unregisterSigIntHandler() {
+        sigaction(SIGINT, &oldSigIntAction, null);
+    }
+
+public:
     /** This func could be used to check if keyboard interrupt happened
       **/
-    @nogc nothrow const(bool) interrupted() {
+    @safe @nogc nothrow const(bool) interrupted() {
         return _interrupted;
     }
 
@@ -39,27 +53,28 @@ public:
       * Also, ensure that deinitSigIntHandling function called when signal
       * handling is not needed anymore.
       **/
-    void initSigIntHandling() {
-        if (_sigint_handler_set)
-            throw new SignalHandlerAlreadySetError(
-                "SIGINT Handler already set!");
+    @safe void initSigIntHandling() {
+        if (_sigint_handler_counter.atomicLoad == 0) {
+            _registerSigIntHandler;
+        }
 
         // Set global variable to indicate that signalling is registered
-        _sigint_handler_set = true;
+        // Each time initSigIntHandling called, this counter will be increased.
+        // Each time deinitSigIntHandling called, this counter will be
+        // decreased.
+        _sigint_handler_counter.atomicOp!"+="(1);
 
-        sigaction_t handler;
-        handler.sa_handler = &_sigIntHandler;
-        handler.sa_mask = cast(sigset_t) 0;
-        handler.sa_flags = 0;
-        handler.sa_flags |= SA_RESTART;
-        sigaction(SIGINT, &handler, &oldSigIntAction);
     }
 
     /** This function must be used to restore SIGINT handling
       * to previous value.
       **/
-    void deinitSigIntHandling() {
-        sigaction(SIGINT, &oldSigIntAction, null);
-        _sigint_handler_set = false;
+    @safe void deinitSigIntHandling() {
+        _sigint_handler_counter.atomicOp!"-="(1);
+
+        // Deinit SigInt handling only if it is last call to this method
+        if (_sigint_handler_counter.atomicLoad == 0) {
+            _unregisterSigIntHandler;
+        }
     }
 
