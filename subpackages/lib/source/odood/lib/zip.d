42e1ec360fa3b65a5322e0bbc8d9750d39665855
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2023-04-02 18:41:33 +0300

    Move parsing of backup manifest to odood.lib.odoo.utils module

--- subpackages/lib/source/odood/lib/zip.d
+++ subpackages/lib/source/odood/lib/zip.d
@@ -6,7 +6,6 @@ private import std.string: toStringz, fromStringz, strip;
 private import std.format: format;
 private import std.algorithm.searching: endsWith, startsWith;
 private import std.path;
-private import std.file;
 private import std.exception: enforce;
 private import std.typecons: Nullable, nullable;
 private import std.json;
@@ -307,44 +306,4 @@ unittest {
     temp_root.join("res", "test-zip", "test-dir", "test-parent.txt").readLink().shouldEqual(
         Path("..", "test.txt"));
     temp_root.join("res", "test-zip", "test-dir", "test-parent.txt").readFileText().shouldEqual("Test Root\n");
-}
-
-
-/** Parse database backup's manifest
-  *
-  * Params:
-  *     path = path to database backup to parse
-  *
-  * Returns: JSONValue that contains parsed database backup manifest
-  **/
-auto parse_database_backup_manifest(in Path path) {
-    int error_code;
-    auto zip_obj = zip_open(path.toStringz, ZIP_RDONLY, &error_code);
-    scope(exit) zip_close(zip_obj);
-
-    enforce!OdoodException(
-        !error_code,
-        "Cannot open zip archive %s for reading: %s".format(
-            path, format_zip_error(error_code)));
-
-    enforce!OdoodException(
-        zip_name_locate(zip_obj, "manifest.json".toStringz, ZIP_FL_ENC_GUESS),
-        "Cannot locate 'manifest.json' inside database backup!");
-
-    auto manifest_file = zip_fopen(zip_obj, "manifest.json".toStringz, ZIP_FL_ENC_GUESS);
-    scope(exit) zip_fclose(manifest_file);
-
-    char[] manifest_content;
-    char[BUF_SIZE] buf;
-    long res;
-    do {
-        res = zip_fread(manifest_file, buf.ptr, BUF_SIZE);
-        manifest_content ~= buf[0..res];
-    } while(res > 0);
-
-    enforce!OdoodException(
-        res == 0,
-        "Cannot read manifest content from database backup!");
-
-    return parseJSON(manifest_content);
 }
