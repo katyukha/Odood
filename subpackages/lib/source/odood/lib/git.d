6b01246ae7d365ce25a736c3675390c0c06e3e37
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2023-04-11 18:33:36 +0300

    Added support for using GitLab CI token implicitly when clonning repos

--- subpackages/lib/source/odood/lib/git.d
+++ subpackages/lib/source/odood/lib/git.d
@@ -78,6 +78,39 @@ private struct GitURL {
         return path_segments;
     }
 
+    /** Apply CI rewrites for Git URL
+      **/
+    auto applyCIRewrites() const {
+        import std.process: environment;
+
+        // If it is not running on gitlab CI at CI_JOB_TOKEN_GIT_HOST,
+        // then no need to apply rewrites
+        if (!environment.get("GITLAB_CI"))
+            return this;
+        if (!environment.get("CI_JOB_TOKEN_GIT_HOST"))
+            return this;
+        if (!environment.get("CI_JOB_TOKEN"))
+            return this;
+        if (environment["CI_JOB_TOKEN_GIT_HOST"] != host)
+            return this;
+
+        GitURL result = this;
+        if ((scheme == "https" || scheme == "http") && !user && !password) {
+            result.user = "gitlab-ci-token";
+            result.password = environment.get("CI_JOB_TOKEN");
+        } else if (scheme == "ssh" && user == "git" && !password) {
+            result.scheme = "https";
+            result.user = "gitlab-ci-token";
+            result.password = environment.get("CI_JOB_TOKEN");
+        } else if (!scheme && !user && !password) {
+            result.scheme = "https";
+            result.user = "gitlab-ci-token";
+            result.password = environment.get("CI_JOB_TOKEN");
+        }
+
+        return result;
+    }
+
     string toString() const {
         return toUrl();
     }
@@ -102,6 +135,16 @@ unittest {
         toUrl.shouldEqual("https://github.com/katyukha/thepath");
     }
 
+    with (GitURL("github.com/katyukha/thepath.git")) {
+        scheme.shouldBeNull;
+        host.shouldEqual("github.com");
+        path.shouldEqual("katyukha/thepath");
+        port.shouldBeNull;
+        user.shouldBeNull;
+        password.shouldBeNull;
+        toUrl.shouldEqual("github.com/katyukha/thepath");
+    }
+
     with (GitURL("git@gitlab.crnd.pro:crnd-opensource/crnd-web.git")) {
         scheme.shouldEqual("ssh");
         host.shouldEqual("gitlab.crnd.pro");
@@ -123,19 +166,107 @@ unittest {
     }
 }
 
-/** TODO: Update git URL according to CI env variables config
-  *
-  *  if [ -n "$CI_JOB_TOKEN_GIT_HOST" ] && [ -n "$GITLAB_CI" ] && [ -n "$CI_JOB_TOKEN" ]; then
-  *      extra_git_opt="$extra_git_opt -c url.\"https://gitlab-ci-token:${CI_JOB_TOKEN}@${CI_JOB_TOKEN_GIT_HOST}/\".insteadOf=\"git@${CI_JOB_TOKEN_GIT_HOST}:\"";
-  *      extra_git_opt="$extra_git_opt -c url.\"https://gitlab-ci-token:${CI_JOB_TOKEN}@${CI_JOB_TOKEN_GIT_HOST}/\".insteadOf=\"https://${CI_JOB_TOKEN_GIT_HOST}/\"";
-  *      extra_git_opt="$extra_git_opt -c url.\"https://gitlab-ci-token:${CI_JOB_TOKEN}@${CI_JOB_TOKEN_GIT_HOST}/\".insteadOf=\"${CI_JOB_TOKEN_GIT_HOST}/\"";
-  *      echoe -e "${BLUEC}Use ${YELLOWC}gitlab-ci-token${BLUEC} for auth to repository ${YELLOWC}${CI_JOB_TOKEN_GIT_HOST}${NC}";
-  *  fi
-  **/
+/// Test CI integration
+unittest {
+    import unit_threaded.assertions;
+    import std.process: environment;
+
+    environment.get("CI_JOB_TOKEN_GIT_HOST").shouldBeNull;
+
+    // CI not configured in env, thus no rewrites have to be applied
+    with (GitURL("https://gitlab.test.com/katyukha/thepath.git").applyCIRewrites) {
+        scheme.shouldEqual("https");
+        host.shouldEqual("gitlab.test.com");
+        path.shouldEqual("katyukha/thepath");
+        port.shouldBeNull;
+        user.shouldBeNull;
+        password.shouldBeNull;
+        toUrl.shouldEqual("https://gitlab.test.com/katyukha/thepath");
+    }
+
+    auto save_env = environment.toAA;
+    environment["GITLAB_CI"] = "1";
+    environment["CI_JOB_TOKEN_GIT_HOST"] = "gitlab.test.com";
+    environment["CI_JOB_TOKEN"] = "gitlab-token-x1";
+    scope(exit) {
+        // Restore env on exit
+        if ("GITLAB_CI" in save_env)
+            environment["GITLAB_CI"] = save_env["GITLAB_CI"];
+        else
+            environment.remove("GITLAB_CI");
+
+        if ("CI_JOB_TOKEN" in save_env)
+            environment["CI_JOB_TOKEN"] = save_env["CI_JOB_TOKEN"];
+        else
+            environment.remove("CI_JOB_TOKEN");
+
+        if ("CI_JOB_TOKEN_GIT_HOST" in save_env)
+            environment["CI_JOB_TOKEN_GIT_HOST"] = save_env["CI_JOB_TOKEN_GIT_HOST"];
+        else
+            environment.remove("CI_JOB_TOKEN_GIT_HOST");
+    }
+
+    with (GitURL("https://gitlab.test.com/katyukha/thepath.git").applyCIRewrites) {
+        scheme.shouldEqual("https");
+        host.shouldEqual("gitlab.test.com");
+        path.shouldEqual("katyukha/thepath");
+        port.shouldBeNull;
+        user.shouldEqual("gitlab-ci-token");
+        password.shouldEqual("gitlab-token-x1");
+        toUrl.shouldEqual(
+            "https://gitlab-ci-token:gitlab-token-x1@gitlab.test.com/katyukha/thepath");
+    }
+
+    with (GitURL("gitlab.test.com/katyukha/thepath.git").applyCIRewrites) {
+        scheme.shouldEqual("https");
+        host.shouldEqual("gitlab.test.com");
+        path.shouldEqual("katyukha/thepath");
+        port.shouldBeNull;
+        user.shouldEqual("gitlab-ci-token");
+        password.shouldEqual("gitlab-token-x1");
+        toUrl.shouldEqual(
+            "https://gitlab-ci-token:gitlab-token-x1@gitlab.test.com/katyukha/thepath");
+    }
+
+    with (GitURL("git@gitlab.test.com:katyukha/thepath.git").applyCIRewrites) {
+        scheme.shouldEqual("https");
+        host.shouldEqual("gitlab.test.com");
+        path.shouldEqual("katyukha/thepath");
+        port.shouldBeNull;
+        user.shouldEqual("gitlab-ci-token");
+        password.shouldEqual("gitlab-token-x1");
+        toUrl.shouldEqual(
+            "https://gitlab-ci-token:gitlab-token-x1@gitlab.test.com/katyukha/thepath");
+    }
+
+    // Other hosts must not be changed
+    with (GitURL("git@gitlab.crnd.pro:crnd-opensource/crnd-web.git").applyCIRewrites) {
+        scheme.shouldEqual("ssh");
+        host.shouldEqual("gitlab.crnd.pro");
+        path.shouldEqual("crnd-opensource/crnd-web");
+        port.shouldBeNull;
+        user.shouldEqual("git");
+        password.shouldBeNull;
+        toUrl.shouldEqual("ssh://git@gitlab.crnd.pro/crnd-opensource/crnd-web");
+    }
+
+    with (GitURL("git@gitlab.crnd.pro:crnd/crnd-account").applyCIRewrites) {
+        scheme.shouldEqual("ssh");
+        host.shouldEqual("gitlab.crnd.pro");
+        path.shouldEqual("crnd/crnd-account");
+        port.shouldBeNull;
+        user.shouldEqual("git");
+        password.shouldBeNull;
+        toUrl.shouldEqual("ssh://git@gitlab.crnd.pro/crnd/crnd-account");
+    }
+}
 
 
 /// Clone git repository to provided destination directory
-void gitClone(in GitURL repo, in Path dest, in string branch,
+void gitClone(
+        in GitURL repo,
+        in Path dest,
+        in string branch,
         in bool single_branch=false) {
     enforce!OdoodException(
         dest.isValid,
@@ -150,7 +281,7 @@ void gitClone(in GitURL repo, in Path dest, in string branch,
     string[] cmd = ["git", "clone", "-b", branch];
     if (single_branch)
         cmd ~= ["--single-branch"];
-    cmd ~= [repo.toUrl, dest.toString];
+    cmd ~= [repo.applyCIRewrites.toUrl, dest.toString];
     cmd.runCmdE;
 }
 
