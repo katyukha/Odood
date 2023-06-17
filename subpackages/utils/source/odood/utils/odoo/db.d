851e19cf3fa4bb06f3c4eab2b27293c5f4b6e196
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2023-06-17 17:16:39 +0300

    Added minimal test for parsing database backup manifest

--- subpackages/utils/source/odood/utils/odoo/db.d
+++ subpackages/utils/source/odood/utils/odoo/db.d
@@ -53,3 +53,20 @@ auto parseDatabaseBackupManifest(in Path path) {
 
     return parseJSON(manifest_content);
 }
+
+
+/// Test parsing database backup manifest
+unittest {
+    import unit_threaded.assertions;
+
+    auto manifest = parseDatabaseBackupManifest(
+        Path("test-data", "demo-db-backup.zip"));
+    manifest["db_name"].get!string.should == "odoo16-odoo-test-backup";
+    manifest["version"].get!string.should == "15.0";
+    manifest["major_version"].get!string.should == "15.0";
+    manifest["pg_version"].get!string.should == "14.0";
+
+    auto manifest_modules = manifest["modules"].object;
+    manifest_modules.length.shouldEqual(37);
+    manifest_modules["crm"].get!string.shouldEqual("15.0.1.6");
+}
