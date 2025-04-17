27c489e8c29c6664c7ef0595979597565bc8efc2
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2025-04-17 22:21:45 +0300

    [Deploy] Improve logrotate config

--- subpackages/lib/source/odood/lib/deploy/templates/logrotate.d
+++ subpackages/lib/source/odood/lib/deploy/templates/logrotate.d
@@ -6,8 +6,10 @@ import odood.lib.project: Project;
 
 
 string generateLogrotateDConfig(in Project project) {
-    // TODO: Set max size and rotate
     return i"$(project.directories.log.toString)/*.log {
+    maxsize 50M
+    rotate 5
+    compress
     copytruncate
     missingok
     notifempty
