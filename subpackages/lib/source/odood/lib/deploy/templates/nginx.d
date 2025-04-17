77e7baa2c92aa7f77fa7909d3ff4c11eb24c50ba
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2025-04-17 22:55:21 +0300

    [Deploy] Minor nginx fixes

--- subpackages/lib/source/odood/lib/deploy/templates/nginx.d
+++ subpackages/lib/source/odood/lib/deploy/templates/nginx.d
@@ -68,9 +68,9 @@ server {
 
     # set headers
     proxy_set_header Host $host;
-    proxy_set_header X-Forwarded-Proto $scheme;
+    proxy_set_header X-Forwarded-Host $host;
     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-    proxy_set_header X-Forward-For $proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto $scheme;
     proxy_set_header X-Real-IP $remote_addr;
 
 
