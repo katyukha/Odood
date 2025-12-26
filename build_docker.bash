621baa48c8c8a2425082ec284dae273eb723cc09
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2025-12-26 21:22:03 +0200

    Simplify docker build process (avoid copying all arg to single image)

--- build_docker.bash
+++ build_docker.bash
@@ -3,6 +3,7 @@
 
 dub build --d-version OdoodInDocker && \
     dub build --d-version OdoodInDocker -c bash-autocomplete && \
-    cp ./build/odood ./docker/bin/odood && \
-    cp ./build/odood.bash ./docker/bin/odood.bash && \
-    (cd ./docker && docker build -t tmp-odood:17 --build-arg ODOO_VERSION=17 --build-arg "ODOOD_DEPENDENCIES=$(cat ../.ci/deps/universal-deb.txt)" .)
+    mkdir -p "./docker/bin/$(dpkg --print-architecture)/" && \
+    cp ./build/odood "./docker/bin/$(dpkg --print-architecture)/odood" && \
+    cp ./build/odood.bash "./docker/bin/$(dpkg --print-architecture)/odood.bash" && \
+    (cd ./docker && docker build -t tmp-odood:18 --build-arg ODOO_VERSION=18 --build-arg "ODOOD_DEPENDENCIES=$(cd .. && .ci/print_deps.d)" .)
