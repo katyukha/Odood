605389b3ad35f1b076fbc15af672fd3e8d967a9b
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2026-03-31 10:13:26 +0300

    [IMP] `odood info` command improved. Now it displays much more info

--- subpackages/cli/source/odood/cli/commands/info.d
+++ subpackages/cli/source/odood/cli/commands/info.d
@@ -1,14 +1,14 @@
 /// Print info about current Odood project
 module odood.cli.commands.info;
 
-private import std.json;
-private import std.stdio: writefln, writeln;
+private import std.json: toJSON;
+private import std.stdio: writeln;
 
-private import colored;
-private import commandr: Option, Flag, ProgramArgs;
+private import commandr: Flag, ProgramArgs;
 
 private import odood.cli.core: OdoodCommand;
 private import odood.lib.project: Project;
+private import odood.lib.project.info: getInfo;
 
 
 class CommandInfo: OdoodCommand {
@@ -17,39 +17,15 @@ class CommandInfo: OdoodCommand {
         this.add(new Flag(null, "json", "Print output in json format"));
     }
 
-    private string[string] prepareInfo(ProgramArgs args, in Project project) const {
-        string[string] res = [
-            "odoo_version": project.odoo.serie.toString,
-            "odoo_branch": project.odoo.branch,
-            "odoo_repository": project.odoo.repo,
-            "python_version": project.venv.py_version.toString,
-        ];
-
-        // TODO: Add postgres version, and other info
-
-        return res;
-    }
-
-    /// Print project info as text
-    private void printInfoText(ProgramArgs args, in Project project) const {
-        foreach(i; prepareInfo(args, project).byKeyValue)
-            writefln("%s: %s", i.key, i.value);
-    }
-
-    /// Print project info as json
-    private void printInfoJSON(ProgramArgs args, in Project project) const {
-        auto info = JSONValue(prepareInfo(args, project));
-        info.toJSON(true).writeln;
-    }
-
     public override void execute(ProgramArgs args) {
         auto project = Project.loadProject;
+        auto info = project.getInfo();
 
-        if (args.flag("json"))
-            printInfoJSON(args, project);
-        else
-            printInfoText(args, project);
+        if (args.flag("json")) {
+            auto json = info.toJSON();
+            json.toJSON(true).writeln;
+        } else {
+            info.toString.writeln;
+        }
     }
 }
-
-
