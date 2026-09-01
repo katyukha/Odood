d935a1d369bef76e578bde4098e309853bda59ec
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2026-09-01 21:09:19 +0300

    Bump python versions and minimal setuptools version

--- subpackages/lib/source/odood/lib/python/odoo.d
+++ subpackages/lib/source/odood/lib/python/odoo.d
@@ -40,16 +40,16 @@ string suggestPythonVersion(in OdooSerie serie) {
     if (serie == OdooSerie(14))
         return "3.8.20";
     if (serie == OdooSerie(15))
-        return "3.10.20";
+        return "3.10.21";
     if (serie == OdooSerie(16))
-        return "3.10.20";
+        return "3.10.21";
     if (serie == OdooSerie(17))
         return "3.11.15";
     if (serie == OdooSerie(18))
-        return "3.12.13";
+        return "3.12.14";
     if (serie == OdooSerie(19))
-        return "3.12.13";
-    return "3.12.13";
+        return "3.12.14";
+    return "3.12.14";
 }
 
 
